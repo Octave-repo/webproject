10fba62f57f1491c82e60f623025a2d598eb91c9
Octave-portfolio <valadieroctave@gmail.com>
2020-09-28 10:18:02 +0200

    Changes to the REST api package

---
+++ api/restjdr/src/main/java/org/projet/restjdr/RestjdrApplication.java
@@ -0,0 +1,13 @@
+package org.projet.restjdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestjdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestjdrApplication.class, args);
+	}
+
+}
---
+++ api/restjdr/src/test/java/org/projet/restjdr/RestjdrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projet.restjdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestjdrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
